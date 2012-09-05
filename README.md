ec6a67b109c4cb881b3f35facfe4a901e9ba169b
Chris Zarate <chris@zarate.org>
2012-09-04 20:40:42 -0400

    Less space than a Nomad.

--- README.md
+++ README.md
@@ -1,4 +1,152 @@
-Sheetrock
-=========
+# jQuery Sheetrock
 
-Quickly connect to, query, and lazy-load data from Google Spreadsheets
+Sheetrock is a flexible jQuery plugin for querying and retrieving data from Google 
+Spreadsheets. It leverages the (powerful) Google Visualization API, which allows 
+filtering, sorting, grouping, and other SQL-like queries.
+
+Sheetrock works with Google Spreadsheets of all sizes—you can retrieve an entire sheet 
+or **load a little bit at a time**. Using the [Google Visualization query language][query], 
+you can construct **custom views** of your data on the fly. It has no dependencies 
+besides jQuery and requires no backend whatsoever—everything is done in the browser 
+using HTML and JavaScript. All you need is the URL of a public Google Spreadsheet to 
+use as a data source.
+
+By default, Sheetrock’s formatting is pretty basic, but customizing the output is 
+easy: just drop in an [Underscore][underscore] or [Handlebars][handlebars] template, 
+or provide your own callback function.
+
+
+## Use
+
+This simple example fetches an entire sheet and populates a table. (The output is 
+truncated here.)
+
+	<table id="statistics"></table>
+	
+	<script>
+		var myOptions = { url: 'https://docs.google.com/spreadsheet/ccc?key=[...]#gid=0' }; 
+		$('#statistics').sheetrock(myOptions);
+	</script>
+
+<table id="statistics"><tbody><tr><th>Team</th><th>Pos</th><th>First</th><th>Last</th><th>Bats</th><th>AB</th><th>R</th><th>H</th><th>HR</th><th>RBI</th><th>SB</th><th>BA</th></tr><tr><td>PHI</td><td>MI</td><td>Luis</td><td>Aguayo</td><td>Right</td><td>133</td><td>17</td><td>28</td><td>4</td><td>13</td><td>1</td><td>0.211</td></tr><tr><td>SFG</td><td>UT</td><td>Mike</td><td>Aldrete</td><td>Left</td><td>216</td><td>27</td><td>54</td><td>2</td><td>25</td><td>1</td><td>0.25</td></tr><tr><td>PIT</td><td>LF</td><td>Bill</td><td>Almon</td><td>Right</td><td>196</td><td>29</td><td>43</td><td>7</td><td>27</td><td>11</td><td>0.219</td></tr><tr><td>LAD</td><td>IF</td><td>Dave</td><td>Anderson</td><td>Right</td><td>216</td><td>31</td><td>53</td><td>1</td><td>15</td><td>5</td><td>0.245</td></tr><tr><td>HOU</td><td>C</td><td>Alan</td><td>Ashby</td><td>Both</td><td>315</td><td>24</td><td>81</td><td>7</td><td>38</td><td>1</td><td>0.257</td></tr><tr><td>NYM</td><td>2B</td><td>Wally</td><td>Backman</td><td>Both</td><td>387</td><td>67</td><td>124</td><td>1</td><td>27</td><td>13</td><td>0.32</td></tr><tr><td>HOU</td><td>C</td><td>Mark</td><td>Bailey</td><td>Both</td><td>153</td><td>9</td><td>27</td><td>4</td><td>15</td><td>1</td><td>0.176</td></tr><tr><td>HOU</td><td>RF</td><td>Kevin</td><td>Bass</td><td>Both</td><td>591</td><td>83</td><td>184</td><td>20</td><td>79</td><td>22</td><td>0.311</td></tr><tr><td>CIN</td><td>3B</td><td>Buddy</td><td>Bell</td><td>Right</td><td>568</td><td>89</td><td>158</td><td>20</td><td>75</td><td>2</td><td>0.278</td></tr><tr><td>PIT</td><td>SS</td><td>Rafael</td><td>Belliard</td><td>Right</td><td>309</td><td>33</td><td>72</td><td>0</td><td>31</td><td>12</td><td>0.233</td></tr><tr><td colspan="12">[...]</td></tr></tbody></table>
+
+---
+
+The entire sheet has hundreds of rows, so loading the whole thing could bog down a 
+page and overwhelm a reader. By passing a `sql` query, we can limit our scope. We can 
+also provide a `chunkSize` to load just a portion of the data. We can always grab more 
+data later—Sheetrock **keeps track of how many rows you’ve requested** and the next 
+request will pick up where you left off.
+
+Let’s look at switch hitters and rank them by batting average. We’ll ignore the other 
+stats and fetch just the top ten to help focus the reader’s attention.
+
+	<table id="statistics"></table>
+	
+	<script>
+
+		var myOptions = {
+			url: 'https://docs.google.com/spreadsheet/ccc?key=[...]#gid=0',
+			sql: "select A,B,C,D,E,L where B = 'Both' order by L desc",
+			chunkSize: 10
+		};
+
+		$('#statistics').sheetrock(myOptions);
+
+	</script>
+
+<table id="statistics"><tbody><tr><th>Team</th><th>Pos</th><th>First</th><th>Last</th><th>Bats</th><th>BA</th></tr><tr><td>MON</td><td>LF</td><td>Tim</td><td>Raines</td><td>Both</td><td>0.334</td></tr><tr><td>NYM</td><td>2B</td><td>Wally</td><td>Backman</td><td>Both</td><td>0.32</td></tr><tr><td>HOU</td><td>RF</td><td>Kevin</td><td>Bass</td><td>Both</td><td>0.311</td></tr><tr><td>CHC</td><td>OF</td><td>Jerry</td><td>Mumphrey</td><td>Both</td><td>0.304</td></tr><tr><td>PIT</td><td>2B</td><td>Johnny</td><td>Ray</td><td>Both</td><td>0.301</td></tr><tr><td>STL</td><td>MI</td><td>Jose</td><td>Oquendo</td><td>Both</td><td>0.297</td></tr><tr><td>MON</td><td>CF</td><td>Mitch</td><td>Webster</td><td>Both</td><td>0.29</td></tr><tr><td>NYM</td><td>OF</td><td>Mookie</td><td>Wilson</td><td>Both</td><td>0.289</td></tr><tr><td>MON</td><td>1B</td><td>Wallace</td><td>Johnson</td><td>Both</td><td>0.283</td></tr><tr><td>STL</td><td>SS</td><td>Ozzie</td><td>Smith</td><td>Both</td><td>0.28</td></tr></tbody></table>
+
+---
+
+Tables are nice, but we might want to represent the data in different ways. Let’s 
+generate an ordered list by passing in an [Underscore][underscore] template. This time 
+we’ll rank the top five players by home runs.
+
+	<h3>NL Home Run Leaders</h3>
+	<ol id="leaders">
+		<script type="text/underscore-template" id="my-template">
+			<li><strong><%= First %> <%= Last %></strong>, <%= Team %>, <%= HR %></li>
+		</script>
+	</ol>
+	
+	<script>
+
+		var myTemplate = _.template($('#my-template').html());
+
+		var myOptions = {
+			url: 'https://docs.google.com/spreadsheet/ccc?key=[...]#gid=0',
+			sql: "select A,C,D,I order by I desc",
+			chunkSize: 5,
+			headers: 1,
+			rowHandler: myTemplate
+		};
+
+		$('#leaders').sheetrock(myOptions);
+
+	</script>
+
+### NL Home Run Leaders
+1. **Mike Schmidt**, PHI, 37
+2. **Glenn Davis**, HOU, 31
+3. **Dave Parker**, CIN, 31
+4. **Dale Murphy**, ATL, 29
+5. **Eric Davis**, CIN, 27
+
+---
+
+You can also group data easily:
+
+	<h3>Team RBI</h3>
+	<ol id="leaders">
+		<script type="text/underscore-template" id="my-template">
+			<li><strong><%= Team %></strong>, <%= RBI %></li>
+		</script>
+	</ol>
+	
+	<script>
+
+		var myTemplate = _.template($('#my-template').html());
+
+		var myOptions = {
+			url: 'https://docs.google.com/spreadsheet/ccc?key=[...]#gid=1',
+			sql: "select A,avg(J) group by A order by avg(J) desc",
+			chunkSize: 5,
+			headers: 1,
+			labels: ['Team', 'Avg'],
+			rowHandler: myTemplate
+		};
+
+		$('#leaders').sheetrock(myOptions);
+
+	</script>
+
+### Team RBI
+1. **NYM**, 686
+2. **PHI**, 638
+3. **CIN**, 614
+4. **SFG**, 595
+5. **SDP**, 591
+
+---
+
+## To-do
+
+* Explain the rest of the advanced options (in the meantime, the source is annotated).
+* Describe how to make a Google Spreadsheet public.
+* Live demo
+* New features?
+
+
+## Credits and license
+
+Sheetrock was inspired by [Tabletop.js][tabletop] (which will teach you jazz piano). 
+[John Brecht][brecht] came up with the name. Sheetrock is released under the MIT 
+license.
+
+
+[query]:      https://developers.google.com/chart/interactive/docs/querylanguage
+[underscore]: http://underscorejs.org
+[handlebars]: http://handlebarsjs.com
+[tabletop]:   http://builtbybalance.com/Tabletop/
+[brecht]:     http://about.me/john.brecht
---
+++ jquery.sheetrock.js
@@ -0,0 +1,357 @@
+/*
+ * jQuery Sheetrock
+ * Quickly connect to, query, and lazy-load data from Google Spreadsheets
+ * http://github.com/chriszarate/Sheetrock
+ */
+
+;(function($) {
+
+  $.fn.sheetrock = function(options) {
+
+    // Load options.
+    options = _options($.extend({}, $.fn.sheetrock.options, options), this);
+
+    // Abort if options were not validated.
+    if(!options) return this;
+
+    // Show loading indicator.
+    if(options.loading) options.loading.show();
+
+    // Enable chunking, if requested, and store offset as jQuery.data.
+    if(options.chunkSize) {
+      options.sql += ' limit ' + (options.chunkSize + 1) + ' offset ' + options.offset;
+      $.data(this[0], _offset, options.offset + options.chunkSize);
+    }
+
+    // Create callback environment and turn on `working` flag.
+    options.callback = 'sheetrock_' + _callbackIndex++;
+    $.fn.sheetrock.working++;
+
+    // Create AJAX request parameters from config options.
+    var params = _params(options);
+
+    // Send request.
+    $.ajax({
+
+      data: params,
+      context: options,
+      url: 'https://spreadsheets.google.com/tq',
+
+      dataType: 'jsonp',
+      cache: true,
+      jsonp: false,
+      jsonpCallback: options.callback,
+
+      success: function(data) {
+
+        // IF valid THEN process ELSE error.
+        if(_validate(data)) {
+          this.dataHandler(data, this);
+        } else {
+          if(this.target) $.data(this.target[0], _error, 1);
+        }
+
+        // Clean up.
+        if(this.loading) this.loading.hide();
+        if(this.userCallback) this.userCallback(this);
+        $.fn.sheetrock.working--;
+
+      }
+
+    });
+
+    return this;
+
+  };
+
+
+  /* Callback index */
+
+  var _callbackIndex = 0;
+
+
+  /* Data parsing */
+
+  // Validate returned data.
+  var _validate = function(data) {
+
+    // Check for successful response types.
+    if(_has(data, 'status') && (data.status === 'ok' || data.status === 'warning') && _has(data, 'table')) {
+
+      // Enumerate any warnings.
+      if(_has(data, 'warnings')) {
+        $.each(data.warnings, function(i, warning) { _log(warning); });
+      }
+
+      return true;
+
+    } else {
+
+      // Enumerate any errors.
+      if(_has(data, 'errors')) {
+        $.each(data.errors, function(i, error) {
+          _log(error.message);
+          _log(error.detailed_message);
+        });
+      }
+
+      return false;
+
+    }
+
+  }
+
+  // Parse data, row by row.
+  var _parse = function(data, options) {
+
+    // Debug returned data
+    // _log(data);
+
+    // The Google API generates an unrecoverable error when the 'offset' 
+    // is larger than the number of available rows. As a workaround, we 
+    // request one more row than we need and stop when we see less rows 
+    // than we requested.
+
+    var last   = (options.chunkSize) ? Math.min(data.table.rows.length, options.chunkSize) : data.table.rows.length;
+    var loaded = (!options.chunkSize || last < options.chunkSize) ? 1 : 0;
+
+    // Store loaded status on target element.
+    if(options.target) $.data(options.target[0], _loaded, loaded);
+
+    // If no column labels are provided (or if there are too many or too 
+    // few), use the returned column labels.
+    var labels = (options.labels && options.labels.length == data.table.cols.length) ? options.labels : _labels(data.table.cols);
+
+    // Output a header row unless told otherwise.
+    if(!options.headers) options.target.append(options.rowHandler(_obj(labels), 0));
+
+    // Each table cell ('c') can contain two properties: 'p' contains 
+    // formatting and 'v' contains the actual field value.
+
+    $.each(data.table.rows, function(i, obj) {
+
+      if(_has(obj, 'c') && i < last) {
+
+        var objData = {}, row = options.offset + i + 1;
+        $.each(obj.c, function(x, field) {
+          var style = (options.formatting) ? _style(field) : false;
+          var value = (field && _has(field, 'v')) ? options.fieldHandler(field.v) : '';
+          objData[labels[x]] = (style) ? _wrap(value, 'span', style) : value;
+        });
+
+        // Pass to row handler and append to target.
+        options.target.append(options.rowHandler(objData, row));
+
+      }
+
+    });
+
+
+  }
+
+
+  /* Validation and assembly */
+
+  // Validate user-passed options.
+  var _options = function(options, target) {
+
+    // Debug options
+    // _log(options);
+
+    // Get spreadsheet key and gid.
+    options.key = _key(options.url) || false;
+    options.gid = _gid(options.url) || false;
+
+    // Validate chunk size and header rows.
+    options.chunkSize = (target.length) ? parseInt(options.chunkSize) || 0 : 0;
+    options.offset = (options.chunkSize) ? parseInt($.data(target[0], _offset)) || 0 : 0;
+    options.headers = parseInt(options.headers) || 0;
+
+    // Add `this` to callback context.
+    options.target = (target.length) ? target : false;
+
+    // Determine if the data is already loaded or previously generated an error.
+    var loaded = (target.length) ? parseInt($.data(target[0], _loaded)) || 0 : 0;
+    var error  = (target.length) ? parseInt($.data(target[0], _error))  || 0 : 0;
+
+    // Make sure `loading` is a jQuery object.
+    if(options.loading && !(options.loading instanceof jQuery)) options.loading = $(options.loading);
+
+    // Require `this` or a handler to receive the data.
+    if(!(target.length || options.dataHandler === _parse)) {
+      _log('Error: No element targeted or data handler provided.');
+      return false;
+    // Abort for finished or error-generating requests.
+    } else if(loaded) {
+      _log('Halt: No more rows to load!');
+      return false;
+    } else if(error) {
+      _log('Halt: A previous request for this resource failed.');
+      return false;
+    // Require a spreadsheet URL.
+    } else if(!options.url) {
+      _log('Error: No spreadsheet URL provided.');
+      return false;
+    // Require a spreadsheet key.
+    } else if(!options.key) {
+      _log('Error: Could not find a key in the provided URL.');
+      return false;
+    // Require a spreadsheet gid.
+    } else if(!options.gid) {
+      _log('Error: Could not find a gid in the provided URL.');
+      return false;
+    }
+
+    return options;
+
+  }
+
+  // Create AJAX request paramater object.
+  var _params = function(options) {
+
+    var params = {
+      key: options.key,
+      gid: options.gid,
+      tqx: 'responseHandler:' + options.callback
+    };
+
+    // Optional SQL request.
+    if(options.sql) params.tq = options.sql;
+
+    return params;
+ 
+  }
+
+
+  /* Miscellaneous functions */
+
+  // Trim string.
+  var _trim = function(str) {
+    return str.toString().replace(/^ +/, '').replace(/ +$/, '');
+  }
+
+  // Shorthand object property lookup.
+  var _has = function(obj, prop) {
+    return (typeof obj[prop] === 'undefined') ? false : true;
+  }
+
+  // Shorthand log to console.
+  var _log = function(item) {
+    console.log(item);
+  }
+
+  // Extract the key from a spreadsheet URL.
+  var _key = function(url) {
+    var keyRegExp = new RegExp('key=([a-z0-9]{30,})&?','i');
+    return (keyRegExp.test(url)) ? url.match(keyRegExp)[1] : false;
+  }
+
+  // Extract the gid from a spreadsheet URL.
+  var _gid = function(url) {
+    var gidRegExp = new RegExp('gid=([0-9]+)','i');
+    return (gidRegExp.test(url)) ? url.match(gidRegExp)[1] : false;
+  }
+
+  // Get column labels from returned data.
+  var _labels = function(cols) {
+    var labels = [];
+    $.each(cols, function(i, obj) {
+      labels[i] = (_has(obj, 'label')) ? obj.label.replace(/ /, '') : obj.id;
+    });
+    return labels;
+  }
+
+  // Convert array to object.
+  var _obj = function(arr) {
+    var obj = {};
+    $.each(arr, function(i, str) { obj[str] = str; });
+    return obj;
+  }
+
+  // Extract formatting from a Google spreadsheet cell.
+  var _style = function(field) {
+    return (field && _has(field, 'p') && _has(field.p, 'style')) ? field.p.style : false;
+  }
+
+  // Output object to HTML (default row handler).
+  var _output = function(obj, i) {
+    var str = '', tag = (i) ? 'td' : 'th';
+    for(prop in obj) str += _wrap(obj[prop], tag, '');
+    return _wrap(str, 'tr', '');
+  }
+
+  // Wrap string in tag.
+  var _wrap = function(str, tag, style) {
+    attr = (style) ? ' style="' + style + '"' : ''
+    return '<' + tag + attr + '>' + str + '</' + tag + '>';
+  }
+
+
+  /* Data labels */
+
+  var _error  = 'sheetrock-error',
+      _loaded = 'sheetrock-loaded',
+      _offset = 'sheetrock-row-offset';
+
+
+  /* Default options */
+
+  $.fn.sheetrock.options = {
+
+    // You must provide, at minimum, the `url` of your spreadsheet. 
+    // Everything else is optional! Defaults can be overridden either 
+    // globally ($.fn.sheetrock.key = value) or per-call using the 
+    // passed options object.
+
+    url:        '',     // String  -- Google spreadsheet URL
+
+    headers:    0,      // Integer -- Number of header rows to skip
+    labels:     [],     // Array   -- Override returned column labels
+    formatting: false,  // Boolean -- Include Google HTML formatting
+    chunkSize:  0,      // Integer -- Number of rows to fetch (0 = all)
+
+    sql:        '',     // String  -- Google Visualization query (SQL-like)
+
+    // Providing a row handler is the recommended way to override the 
+    // default data formatting. This function should accept a row object 
+    // and a row number (header = 0, first data row = 1). The properties 
+    // of this object will be named after its column labels. Your function 
+    // should return content (a DOM/jQuery object or an HTML string) that 
+    // is ready to be appended to your target element. A very easy way to 
+    // do this is to provide an Underscore.js template (which is itself a 
+    // function).
+
+    rowHandler: _output,  // Function
+
+    // This function is used to process every cell value. It should return 
+    // a string. The provided default is a simple trim function.
+
+    fieldHandler: _trim,  // Function
+
+    // Providing your own data handler means you don't want any processing 
+    // to take place except for basic validation. The returned data, if 
+    // valid, is passed to your data handler (along with an options hash) 
+    // and it will be completely up to you to do something with it. The 
+    // field handler and row handler functions will not be called.
+
+    dataHandler: _parse,  // Function
+
+    // You can provide a function to be called when all processing is 
+    // complete. The options hash is passed to this function.
+
+    userCallback: false,  // Function
+
+    // If you have a loading indicator in your page, provide a jQuery object 
+    // or selector here. It will be shown when the request starts and hidden 
+    // when it ends.
+
+    loading: false  // jQuery object or selector
+
+  };
+
+  // This property is set to the number of active requests. This can be useful 
+  // for monitoring or for infinite scroll bindings.
+
+  $.fn.sheetrock.working = 0;
+
+})(jQuery);
