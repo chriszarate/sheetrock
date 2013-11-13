30a787e294ff53a6068003ef475159fc76a4cfe3
chriszarate <chris@zarate.org>
2013-11-12 23:16:04 -0500

    Bump version number to 0.1.7.

--- src/jquery.sheetrock.js
+++ src/jquery.sheetrock.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Sheetrock v0.1.6
+ * jQuery Sheetrock v0.1.7
  * Quickly connect to, query, and lazy-load data from Google Spreadsheets
  * Requires jQuery >=1.6
  * http://chriszarate.github.io/sheetrock/
@@ -691,6 +691,6 @@
   $.fn.sheetrock.promise = $.Deferred().resolve();
 
   // Version number.
-  $.fn.sheetrock.version = '0.1.6';
+  $.fn.sheetrock.version = '0.1.7';
 
 });
