0f0c09f30222f4290fda086d64f815df26b00c7e
chriszarate <chris@zarate.org>
2015-03-17 00:20:31 -0400

    Reorder functions to avoid latedef.

--- src/jquery.sheetrock.js
+++ src/jquery.sheetrock.js
@@ -47,41 +47,134 @@
   // JSONP callback function index
   var jsonpCallbackIndex = 0;
 
-  // Fetch the requested data using the user's options.
-  var fetchData = function (options) {
 
-    // Specify a custom callback function since Google doesn't use the
-    // default implementation favored by jQuery.
-    var jsonpCallbackName = 'sheetrock_callback_' + jsonpCallbackIndex;
-    jsonpCallbackIndex = jsonpCallbackIndex + 1;
+  /* Helpers */
 
-    // AJAX request options
-    var request = {
+  // General error handler.
+  var handleError = function (options, data, msg) {
 
-      // Convert user options into AJAX request parameters.
-      data: {
-        gid: options.gid,
-        tq: options.query,
-        tqx: 'responseHandler:' + jsonpCallbackName
-      },
+    // Set error message.
+    msg = msg || 'Request failed.';
 
-      url: options.apiEndpoint,
-      dataType: 'jsonp',
-      cache: true,
+    // Remember that this request failed.
+    if (options && options.requestID) {
+      requestStatusCache.failed[options.requestID] = true;
+    }
 
-      // Use custom callback function (see above).
-      jsonp: false,
-      jsonpCallback: jsonpCallbackName
+    // Call the user's error handler.
+    options.errorHandler(options, data, msg);
 
-    };
+    throw msg;
 
-    // If debugging is enabled, log request details to the console.
-    log(request, options.debug);
+  };
 
-    // Send the request.
-    $.ajax(request)
-      .done(createClosure(processResponse, options))
-      .fail(createClosure(handleError, options));
+  // Trim a string of leading and trailing spaces.
+  var trim = function (str) {
+    return str.toString().replace(/^ +/, '').replace(/ +$/, '');
+  };
+
+  // Parse a string as a natural number (>=0).
+  var stringToNaturalNumber = function (str) {
+    return Math.max(0, parseInt(str, 10) || 0);
+  };
+
+  // Return true if all of the passed arguments are defined.
+  var isDefined = function () {
+    var i;
+    var length = arguments.length;
+    for (i = 0; i < length; i = i + 1) {
+      if (typeof arguments[i] === 'undefined') {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // Return true if an object has all of the passed arguments as properties.
+  var has = function (obj) {
+    var i;
+    var length = arguments.length;
+    for (i = 1; i < length; i = i + 1) {
+      if (!isDefined(obj[arguments[i]])) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // Log something to the browser console, if it exists. The argument "show"
+  // is a Boolean (default = true) that determines whether to proceed.
+  var log = function (msg, show) {
+    /* jshint devel: true */
+    show = (isDefined(show, console)) ? show : true;
+    if (show && console.log) {
+      console.log(msg);
+    }
+    return false;
+  };
+
+  // Get API endpoint, key, and gid from a Google Sheet URL.
+  var getSheetParams = function (url) {
+
+    var sheetParams = {};
+
+    $.each(sheetTypes, function (typeKey, sheetType) {
+      if (sheetType.keyFormat.test(url) && sheetType.gidFormat.test(url)) {
+        sheetParams.key = url.match(sheetType.keyFormat)[1];
+        sheetParams.gid = url.match(sheetType.gidFormat)[1];
+        sheetParams.apiEndpoint = sheetType.apiEndpoint.replace('%key%', sheetParams.key);
+        return false;
+      }
+    });
+
+    return sheetParams;
+
+  };
+
+  // Extract the label, if present, from a column object, sans white space.
+  var getColumnLabel = function (col) {
+    return (has(col, 'label')) ? col.label.replace(/\s/g, '') : null;
+  };
+
+  // Map function: Return the label or letter of a column object.
+  var getColumnLabelOrLetter = function (col) {
+    return getColumnLabel(col) || col.id;
+  };
+
+  // Convert an array to a object.
+  var arrayToObject = function (arr) {
+    var obj = {};
+    $.each(arr, function (i, str) {
+      obj[str] = str;
+    });
+    return obj;
+  };
+
+  // Wrap a string in tag. The style argument, if present, is populated into
+  // an inline CSS style attribute. (Gross!)
+  var wrapTag = function (str, tag) {
+    return '<' + tag + '>' + str + '</' + tag + '>';
+  };
+
+  // Default row handler: Output a row object as an HTML table row.
+  var toHTML = function (row) {
+
+    var cell;
+    var html = '';
+
+    // Use "td" for table body row, "th" for table header rows.
+    var tag = (row.num) ? 'td' : 'th';
+
+    // Loop through each cell in the row.
+    for (cell in row.cells) {
+      if (row.cells.hasOwnProperty(cell)) {
+        // Wrap the cell value in the cell tag.
+        html += wrapTag(row.cells[cell], tag);
+      }
+    }
+
+    // Wrap the cells in a table row tag.
+    return wrapTag(html, 'tr');
 
   };
 
@@ -92,53 +185,76 @@
     };
   };
 
-  // Enumerate any messages embedded in the API response.
-  var enumerateMessages = function (data, state) {
 
-    // Look for the specified property at the root of the response object.
-    if (has(data, state)) {
+  /* Options */
 
-      // Look for the kinds of messages we know about.
-      $.each(data[state], function (i, status) {
-        if (has(status, 'detailed_message')) {
-          /* jshint camelcase: false */
-          // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-          log(status.detailed_message);
-          // jscs:enable
-        } else if (has(status, 'message')) {
-          log(status.message);
-        }
-      });
+  // Validate user-passed options.
+  var validateOptions = function (options) {
+
+    // Extend default options.
+    options = $.extend({}, $.fn.sheetrock.defaults, getSheetParams(options.url), options);
+
+    // Support some legacy option names.
+    options.query = options.sql || options.query;
+    options.reset = options.resetStatus || options.reset;
+    options.callback = options.userCallback || options.callback;
+
+    // Set request ID (key_gid_query).
+    options.requestID = options.key + '_' + options.gid + '_' + options.query;
+
+    // Validate chunk size.
+    options.chunkSize = (options.target.length) ? stringToNaturalNumber(options.chunkSize) : 0;
+
+    // Validate number of header rows.
+    options.headers = stringToNaturalNumber(options.headers);
+
+    // If requested, reset request status.
+    if (options.reset && options.requestID) {
+      requestStatusCache.loaded[options.requestID] = false;
+      requestStatusCache.failed[options.requestID] = false;
+      requestStatusCache.offset[options.requestID] = 0;
+      log('Resetting request status.');
+    }
+
+    // Retrieve current row offset.
+    options.offset = requestStatusCache.offset[options.requestID] || 0;
+
+    // If requested, make a request for chunked data.
+    if (options.chunkSize && options.target && options.requestID) {
+
+      // Append a limit and row offest to the query to target the next chunk.
+      options.query += ' limit ' + (options.chunkSize + 1);
+      options.query += ' offset ' + options.offset;
+
+      // Remember the new row offset.
+      requestStatusCache.offset[options.requestID] = options.offset + options.chunkSize;
 
     }
 
-  };
-
-  // Validate API response.
-  var processResponse = function (options, data) {
-
-    enumerateMessages(data, 'warnings');
-    enumerateMessages(data, 'errors');
-
-    log(data, options.debug);
-
-    // Make sure the response is populated with actual data.
-    if (has(data, 'status', 'table') && has(data.table, 'cols', 'rows')) {
-
-      // Extend the options hash with useful information about the response.
-      options = extendOptions(options, data);
-
-      // If there is an element being targeted, parse the data into HTML.
-      if (options.target.length) {
-        parseData(options, data);
-      }
-
-      // Call the user's callback function.
-      options.callback(options, data);
-
-    } else {
-      handleError(options, data, 'Unexpected API response format.');
+    // Require `this` or a callback function. Otherwise, the data has nowhere to go.
+    if (!options.target.length && options.callback === $.noop) {
+      handleError(options, null, 'No element targeted or callback provided.');
     }
+
+    // Require a Sheet key and gid.
+    if (!options.key || !options.gid) {
+      handleError(options, null, 'No key/gid in the provided URL.');
+    }
+
+    // Abandon requests that have previously generated an error.
+    if (requestStatusCache.failed[options.requestID]) {
+      handleError(options, null, 'A previous request for this resource failed.');
+    }
+
+    // Abandon requests that have already been loaded.
+    if (requestStatusCache.loaded[options.requestID]) {
+      handleError(options, null, 'No more rows to load!');
+    }
+
+    // Log the validated options to the console, if requested.
+    log(options, options.debug);
+
+    return options;
 
   };
 
@@ -172,6 +288,31 @@
 
     // Return extended options.
     return options;
+
+  };
+
+
+  /* Data */
+
+  // Enumerate any messages embedded in the API response.
+  var enumerateMessages = function (data, state) {
+
+    // Look for the specified property at the root of the response object.
+    if (has(data, state)) {
+
+      // Look for the kinds of messages we know about.
+      $.each(data[state], function (i, status) {
+        if (has(status, 'detailed_message')) {
+          /* jshint camelcase: false */
+          // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+          log(status.detailed_message);
+          // jscs:enable
+        } else if (has(status, 'message')) {
+          log(status.message);
+        }
+      });
+
+    }
 
   };
 
@@ -254,202 +395,70 @@
 
   };
 
-  // Validate user-passed options.
-  var validateOptions = function (options) {
+  // Validate API response.
+  var processResponse = function (options, data) {
 
-    // Extend default options.
-    options = $.extend({}, $.fn.sheetrock.defaults, getSheetParams(options.url), options);
+    enumerateMessages(data, 'warnings');
+    enumerateMessages(data, 'errors');
 
-    // Support some legacy option names.
-    options.query = options.sql || options.query;
-    options.reset = options.resetStatus || options.reset;
-    options.callback = options.userCallback || options.callback;
+    log(data, options.debug);
 
-    // Set request ID (key_gid_query).
-    options.requestID = options.key + '_' + options.gid + '_' + options.query;
+    // Make sure the response is populated with actual data.
+    if (has(data, 'status', 'table') && has(data.table, 'cols', 'rows')) {
 
-    // Validate chunk size.
-    options.chunkSize = (options.target.length) ? stringToNaturalNumber(options.chunkSize) : 0;
+      // Extend the options hash with useful information about the response.
+      options = extendOptions(options, data);
 
-    // Validate number of header rows.
-    options.headers = stringToNaturalNumber(options.headers);
-
-    // If requested, reset request status.
-    if (options.reset && options.requestID) {
-      requestStatusCache.loaded[options.requestID] = false;
-      requestStatusCache.failed[options.requestID] = false;
-      requestStatusCache.offset[options.requestID] = 0;
-      log('Resetting request status.');
-    }
-
-    // Retrieve current row offset.
-    options.offset = requestStatusCache.offset[options.requestID] || 0;
-
-    // If requested, make a request for chunked data.
-    if (options.chunkSize && options.target && options.requestID) {
-
-      // Append a limit and row offest to the query to target the next chunk.
-      options.query += ' limit ' + (options.chunkSize + 1);
-      options.query += ' offset ' + options.offset;
-
-      // Remember the new row offset.
-      requestStatusCache.offset[options.requestID] = options.offset + options.chunkSize;
-
-    }
-
-    // Require `this` or a callback function. Otherwise, the data has nowhere to go.
-    if (!options.target.length && options.callback === $.noop) {
-      handleError(options, null, 'No element targeted or callback provided.');
-    }
-
-    // Require a Sheet key and gid.
-    if (!options.key || !options.gid) {
-      handleError(options, null, 'No key/gid in the provided URL.');
-    }
-
-    // Abandon requests that have previously generated an error.
-    if (requestStatusCache.failed[options.requestID]) {
-      handleError(options, null, 'A previous request for this resource failed.');
-    }
-
-    // Abandon requests that have already been loaded.
-    if (requestStatusCache.loaded[options.requestID]) {
-      handleError(options, null, 'No more rows to load!');
-    }
-
-    // Log the validated options to the console, if requested.
-    log(options, options.debug);
-
-    return options;
-
-  };
-
-  // General error handler.
-  var handleError = function (options, data, msg) {
-
-    // Set error message.
-    msg = msg || 'Request failed.';
-
-    // Remember that this request failed.
-    if (options && options.requestID) {
-      requestStatusCache.failed[options.requestID] = true;
-    }
-
-    // Call the user's error handler.
-    options.errorHandler(options, data, msg);
-
-    throw msg;
-
-  };
-
-  // Trim a string of leading and trailing spaces.
-  var trim = function (str) {
-    return str.toString().replace(/^ +/, '').replace(/ +$/, '');
-  };
-
-  // Parse a string as a natural number (>=0).
-  var stringToNaturalNumber = function (str) {
-    return Math.max(0, parseInt(str, 10) || 0);
-  };
-
-  // Return true if an object has all of the passed arguments as properties.
-  var has = function (obj) {
-    var i;
-    var length = arguments.length;
-    for (i = 1; i < length; i = i + 1) {
-      if (!isDefined(obj[arguments[i]])) {
-        return false;
+      // If there is an element being targeted, parse the data into HTML.
+      if (options.target.length) {
+        parseData(options, data);
       }
-    }
-    return true;
-  };
 
-  // Return true if all of the passed arguments are defined.
-  var isDefined = function () {
-    var i;
-    var length = arguments.length;
-    for (i = 0; i < length; i = i + 1) {
-      if (typeof arguments[i] === 'undefined') {
-        return false;
-      }
-    }
-    return true;
-  };
+      // Call the user's callback function.
+      options.callback(options, data);
 
-  // Log something to the browser console, if it exists. The argument "show"
-  // is a Boolean (default = true) that determines whether to proceed.
-  var log = function (msg, show) {
-    /* jshint devel: true */
-    show = (isDefined(show, console)) ? show : true;
-    if (show && console.log) {
-      console.log(msg);
-    }
-    return false;
-  };
-
-  // Get API endpoint, key, and gid from a Google Sheet URL.
-  var getSheetParams = function (url) {
-
-    var sheetParams = {};
-
-    $.each(sheetTypes, function (typeKey, sheetType) {
-      if (sheetType.keyFormat.test(url) && sheetType.gidFormat.test(url)) {
-        sheetParams.key = url.match(sheetType.keyFormat)[1];
-        sheetParams.gid = url.match(sheetType.gidFormat)[1];
-        sheetParams.apiEndpoint = sheetType.apiEndpoint.replace('%key%', sheetParams.key);
-        return false;
-      }
-    });
-
-    return sheetParams;
-
-  };
-
-  // Extract the label, if present, from a column object, sans white space.
-  var getColumnLabel = function (col) {
-    return (has(col, 'label')) ? col.label.replace(/\s/g, '') : null;
-  };
-
-  // Map function: Return the label or letter of a column object.
-  var getColumnLabelOrLetter = function (col) {
-    return getColumnLabel(col) || col.id;
-  };
-
-  // Convert an array to a object.
-  var arrayToObject = function (arr) {
-    var obj = {};
-    $.each(arr, function (i, str) {
-      obj[str] = str;
-    });
-    return obj;
-  };
-
-  // Default row handler: Output a row object as an HTML table row.
-  var toHTML = function (row) {
-
-    var cell;
-    var html = '';
-
-    // Use "td" for table body row, "th" for table header rows.
-    var tag = (row.num) ? 'td' : 'th';
-
-    // Loop through each cell in the row.
-    for (cell in row.cells) {
-      if (row.cells.hasOwnProperty(cell)) {
-        // Wrap the cell value in the cell tag.
-        html += wrapTag(row.cells[cell], tag);
-      }
+    } else {
+      handleError(options, data, 'Unexpected API response format.');
     }
 
-    // Wrap the cells in a table row tag.
-    return wrapTag(html, 'tr');
-
   };
 
-  // Wrap a string in tag. The style argument, if present, is populated into
-  // an inline CSS style attribute. (Gross!)
-  var wrapTag = function (str, tag) {
-    return '<' + tag + '>' + str + '</' + tag + '>';
+  // Fetch the requested data using the user's options.
+  var fetchData = function (options) {
+
+    // Specify a custom callback function since Google doesn't use the
+    // default implementation favored by jQuery.
+    var jsonpCallbackName = 'sheetrock_callback_' + jsonpCallbackIndex;
+    jsonpCallbackIndex = jsonpCallbackIndex + 1;
+
+    // AJAX request options
+    var request = {
+
+      // Convert user options into AJAX request parameters.
+      data: {
+        gid: options.gid,
+        tq: options.query,
+        tqx: 'responseHandler:' + jsonpCallbackName
+      },
+
+      url: options.apiEndpoint,
+      dataType: 'jsonp',
+      cache: true,
+
+      // Use custom callback function (see above).
+      jsonp: false,
+      jsonpCallback: jsonpCallbackName
+
+    };
+
+    // If debugging is enabled, log request details to the console.
+    log(request, options.debug);
+
+    // Send the request.
+    $.ajax(request)
+      .done(createClosure(processResponse, options))
+      .fail(createClosure(handleError, options));
+
   };
 
 
