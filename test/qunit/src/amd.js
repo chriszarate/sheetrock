c0cb9ef06b69e270290f2a9ccdeb9bb0c7639f19
chriszarate <chris@zarate.org>
2015-03-22 16:44:06 -0400

    Require QUnit where possible and DRY out some tests.

--- test/qunit/src/amd.js
+++ test/qunit/src/amd.js
@@ -1,9 +1,8 @@
 /*global define, window, QUnit */
-/*jslint indent: 2, vars: true */
 
 'use strict';
 
-define(['../../../src/sheetrock'], function (sheetrock) {
+define(['../../../src/sheetrock', './common'], function (sheetrock, commonTests) {
 
   QUnit.module('Load Sheetrock with AMD.');
 
@@ -12,28 +11,6 @@ define(['../../../src/sheetrock'], function (sheetrock) {
     assert.ok(window.sheetrock === undefined, 'Sheetrock global is undefined.');
   });
 
-  QUnit.test('Load a legacy-format spreadsheet.', function (assert) {
-
-    var done = assert.async();
-
-    assert.expect(5);
-
-    sheetrock({
-      url: 'https://docs.google.com/spreadsheet/ccc?key=0AlRp2ieP7izLdGFNOERTZW0xLVpROFc3X3FJQ2tSb2c#gid=0',
-      query: 'select A,B,C,D,E,L where E = \'Both\' order by L desc',
-      callback: function (error, options, data) {
-        if (!error) {
-          assert.ok(/tqx=responseHandler:/.test(options.request.url), 'Request URL used JSONP transport.');
-          assert.ok(data.status === 'ok', 'Valid response from Google API.');
-          assert.ok(data.table.cols.length === 6, 'Response contains 6 columns.');
-          assert.ok(data.table.rows.length === 35, 'Response contains 35 rows.');
-          assert.ok(data.table.rows[2].c[3].v === 'Bass', 'Row 3, column 4 contains "Bass".');
-          done();
-        }
-      }
-
-    });
-
-  });
+  commonTests(QUnit, sheetrock);
 
 });
--- test/qunit/src/browserify.js
+++ test/qunit/src/browserify.js
@@ -1,36 +1,16 @@
-/*global window, QUnit */
-/*jslint indent: 2, node: true, vars: true */
+/*global window */
 
 'use strict';
 
+var qunit = require('qunitjs');
 var sheetrock = require('../../../src/sheetrock');
+var commonTests = require('./common');
 
-QUnit.module('Load Sheetrock with Browserify.');
+qunit.module('Load Sheetrock with Browserify.');
 
-QUnit.test('Test global', function (assert) {
+qunit.test('Test global', function (assert) {
   assert.expect(1);
   assert.ok(window.sheetrock === undefined, 'Sheetrock global is undefined.');
 });
 
-QUnit.test('Load a legacy-format spreadsheet.', function (assert) {
-
-  var done = assert.async();
-
-  assert.expect(5);
-
-  sheetrock({
-    url: 'https://docs.google.com/spreadsheet/ccc?key=0AlRp2ieP7izLdGFNOERTZW0xLVpROFc3X3FJQ2tSb2c#gid=0',
-    query: 'select A,B,C,D,E,L where E = \'Both\' order by L desc',
-    callback: function (error, options, data) {
-      if (!error) {
-        assert.ok(/tqx=responseHandler:/.test(options.request.url), 'Request URL used JSONP transport.');
-        assert.ok(data.status === 'ok', 'Valid response from Google API.');
-        assert.ok(data.table.cols.length === 6, 'Response contains 6 columns.');
-        assert.ok(data.table.rows.length === 35, 'Response contains 35 rows.');
-        assert.ok(data.table.rows[2].c[3].v === 'Bass', 'Row 3, column 4 contains "Bass".');
-        done();
-      }
-    }
-  });
-
-});
+commonTests(qunit, sheetrock);
