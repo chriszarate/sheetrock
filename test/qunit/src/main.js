0a42a4db18a3a024106fcafec2688ecc9f9c7869
chriszarate <chris@zarate.org>
2015-04-12 13:31:01 -0400

    Run QUnit tests with phantomjs test runner.

--- test/qunit/src/main.js
+++ test/qunit/src/main.js
@@ -1,10 +1,28 @@
 'use strict';
 
+var exec = require('child_process').exec;
 var glob = require('glob');
-var qunit = require('node-qunit-phantomjs');
+
+var phantomjs = 'node_modules/phantomjs/bin/phantomjs';
+var options = ' --web-security=false';
+var runScript = 'node_modules/phantomjs/lib/phantom/examples/run-qunit.js';
+var exitCode = 0;
 
 glob('test/qunit/*.html', function (error, files) {
+
+  var fileCount = files.length;
+
   files.forEach(function (file) {
-    qunit(file);
+    exec(phantomjs + options + ' ' + runScript + ' ' + file, function (error, stdout, stderr) {
+      fileCount = fileCount - 1;
+      console.log(stdout, stderr);
+      if (error || stderr) {
+        exitCode = 1;
+      }
+      if (fileCount === 0) {
+        process.exit(exitCode);
+      }
+    });
   });
+
 });
