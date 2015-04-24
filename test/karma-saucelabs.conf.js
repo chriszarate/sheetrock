f6426a619fc43d989d559e90576fb7de97573670
chriszarate <chris@zarate.org>
2015-04-24 08:46:29 -0400

    Reduce full test suite to automatically selected latest versions.

--- test/karma-saucelabs.conf.js
+++ test/karma-saucelabs.conf.js
@@ -5,76 +5,31 @@ var sharedConfig = require('./karma-shared.conf.js');
 /*jshint camelcase: false*/
 /*jscs: disable requireCamelCaseOrUpperCaseIdentifiers */
 var browsers = {
-  sl_android_5_1: {
+  sl_android_latest: {
     base: 'SauceLabs',
     browserName: 'android',
-    deviceName: 'Android Emulator',
-    platform: 'Linux',
-    version: '5.1'
+    deviceName: 'Android Emulator'
   },
-  sl_android_4_4: {
+  sl_chrome_latest: {
     base: 'SauceLabs',
-    browserName: 'android',
-    deviceName: 'Android Emulator',
-    platform: 'Linux',
-    version: '4.4'
+    browserName: 'chrome'
   },
-  sl_chrome_40: {
+  sl_firefox_latest: {
     base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 8.1',
-    version: '40.0'
+    browserName: 'firefox'
   },
-  sl_firefox_30: {
+  sl_ie_latest: {
     base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 7',
-    version: '30.0'
+    browserName: 'internet explorer'
   },
-  sl_firefox_20: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 7',
-    version: '20.0'
-  },
-  sl_ie_11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
-  },
-  sl_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '10'
-  },
-  sl_ios_safari_8: {
+  sl_ios_safari_latest: {
     base: 'SauceLabs',
     browserName: 'iphone',
-    deviceName: 'iPhone Simulator',
-    platform: 'OS X 10.10',
-    version: '8.2'
+    deviceName: 'iPhone Simulator'
   },
-  sl_ios_safari_7: {
+  sl_safari_latest: {
     base: 'SauceLabs',
-    browserName: 'iphone',
-    deviceName: 'iPhone Simulator',
-    platform: 'OS X 10.10',
-    version: '7.1'
-  },
-  sl_ios_safari_6: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    deviceName: 'iPhone Simulator',
-    platform: 'OS X 10.10',
-    version: '6.1'
-  },
-  sl_safari_8: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.10',
-    version: '8.0'
+    browserName: 'safari'
   }
 };
 
