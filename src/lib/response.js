eb37896c6ff119ed8e800c49468896769cf5b8af
chriszarate <chris@zarate.org>
2021-09-12 17:53:51 -0400

    Trigger a custom event when the data is appended

--- src/lib/response.js
+++ src/lib/response.js
@@ -118,6 +118,7 @@ export default class Response {
     }
 
     util.append(target, headerHTML + bodyHTML);
+    util.triggerEvent(target, 'sheetrock:loaded');
 
     this.html = headerHTML + bodyHTML;
   }
