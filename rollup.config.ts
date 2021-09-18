c09bb4efad7fcfb61cc17bc20933f0f8e9d5cf6d
chriszarate <chris@zarate.org>
2021-09-18 14:13:48 -0400

    Revert "Remove Rollup"
    This reverts commit e033e96b7c8c657195fabb66aa87732c0348bf92.

---
+++ rollup.config.ts
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/sheetrock.min.js',
+      format: 'umd',
+      name: 'sheetrock',
+    },
+  ],
+  plugins: [terser(), typescript()],
+};
